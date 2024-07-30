acd617df5248376c33782345aebebb340e6daac8
RanukaDinsitha <ranuka@outlook.co.nz>
2024-07-30 19:25:20 +1200

    errors everywhere

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pygame
-from os.path import join 
+from os.path import join as path
 
 pygame.init()
-clock = pygame.time.Clock
-icon = join(".", "assets", "icon.png")
+clock = pygame.time.Clock()
+icon = path(".", "assets", "icon.png")
+running = True
+
 pygame.display.set_caption('Bluestroid')
 pygame.display.set_icon(icon)
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
